7ec9654449d9fd19dce113f7484fdbab88dcc66f
patrickmark.mazo <dev.patmazo@gmail.com>
2023-12-12 22:13:47 +0800

    sample unit test table driven

--- test/access_test.go
+++ test/access_test.go
@@ -8,4 +8,100 @@
 // Role : Senior Software Engineer
 package test
 
-func DominoAccessTest()
+import (
+	"testing"
+
+	gosdk "github.com/HCL-TECH-SOFTWARE/domino-rest-sdk-go"
+)
+
+func TestAccessCredentials(t *testing.T) {
+
+	var testCases = []struct {
+		title    string
+		input    *gosdk.Config
+		expected string
+	}{
+		{
+			title:    "TestCase 1: Should fail if BaseURL is empty.",
+			expected: "URL should not be empty.",
+			input: &gosdk.Config{
+				BaseUrl: "",
+				Credentials: gosdk.Credentials{
+					Scope:        "",
+					Type:         "",
+					UserName:     "",
+					Password:     "",
+					AppID:        "",
+					AppSecret:    "",
+					Token:        "",
+					RefreshToken: "",
+					ExpiryTime:   0,
+				},
+			},
+		},
+		{
+			title:    "TestCase 2: Should fail if config type is empty.",
+			expected: "OAUTH needs appSecret, appId and refreshToken",
+			input: &gosdk.Config{
+				BaseUrl: "https://frascati.projectkeep.io",
+				Credentials: gosdk.Credentials{
+					Scope:        "",
+					Type:         "",
+					UserName:     "",
+					Password:     "",
+					AppID:        "",
+					AppSecret:    "",
+					Token:        "",
+					RefreshToken: "",
+					ExpiryTime:   0,
+				},
+			},
+		},
+		{
+			title:    "TestCase 3: Should fail if type is BASIC and no username",
+			expected: "BASIC authentication needs username and password.",
+			input: &gosdk.Config{
+				BaseUrl: "https://frascati.projectkeep.io",
+				Credentials: gosdk.Credentials{
+					Scope:        "",
+					Type:         "BASIC",
+					UserName:     "",
+					Password:     "",
+					AppID:        "",
+					AppSecret:    "",
+					Token:        "",
+					RefreshToken: "",
+					ExpiryTime:   0,
+				},
+			},
+		},
+		{
+			title:    "TestCase 4: Should fail if type is BASIC and no password",
+			expected: "BASIC authentication needs username and password.",
+			input: &gosdk.Config{
+				BaseUrl: "https://frascati.projectkeep.io",
+				Credentials: gosdk.Credentials{
+					Scope:        "",
+					Type:         "BASIC",
+					UserName:     "username",
+					Password:     "",
+					AppID:        "",
+					AppSecret:    "",
+					Token:        "",
+					RefreshToken: "",
+					ExpiryTime:   0,
+				},
+			},
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.title, func(t *testing.T) {
+			_, err := testCase.input.DominoAccess()
+			if err.Error() != testCase.expected {
+				t.Errorf("%s, Result: %s, Expected: %s", testCase.title, err.Error(), testCase.expected)
+			}
+		})
+	}
+
+}
